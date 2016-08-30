ce9ce9f7c53465bfc458e50ecbe25fa2aef9defb
Sergei <hmaster20@gmail.com>
2016-08-30 19:09:17 +0300

    draft

--- Dictionary/Main.Designer.cs
+++ Dictionary/Main.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.создатьБазуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -39,23 +40,33 @@
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.Edit = new System.Windows.Forms.Button();
             this.dgvTable = new System.Windows.Forms.DataGridView();
-            this.Delete = new System.Windows.Forms.Button();
-            this.Add = new System.Windows.Forms.Button();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.button1 = new System.Windows.Forms.Button();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.tssLabel = new System.Windows.Forms.ToolStripStatusLabel();
-            this.tscbTypeFilter = new System.Windows.Forms.ComboBox();
-            this.tscbSort = new System.Windows.Forms.ComboBox();
             this.colWordEn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colWordRu = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tssLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btRun = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.rbSelect = new System.Windows.Forms.RadioButton();
+            this.rbInsert = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.menuStrip1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvTable)).BeginInit();
             this.tabPage2.SuspendLayout();
             this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -65,7 +76,7 @@
             this.настройкаToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(909, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(930, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -117,14 +128,12 @@
             this.tabControl1.Location = new System.Drawing.Point(0, 24);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(909, 462);
+            this.tabControl1.Size = new System.Drawing.Size(930, 462);
             this.tabControl1.TabIndex = 1;
             // 
             // tabPage1
             // 
             this.tabPage1.BackColor = System.Drawing.SystemColors.Control;
-            this.tabPage1.Controls.Add(this.tscbSort);
-            this.tabPage1.Controls.Add(this.tscbTypeFilter);
             this.tabPage1.Controls.Add(this.Edit);
             this.tabPage1.Controls.Add(this.dgvTable);
             this.tabPage1.Controls.Add(this.Delete);
@@ -132,13 +141,13 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(901, 436);
+            this.tabPage1.Size = new System.Drawing.Size(922, 436);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Словарь";
             // 
             // Edit
             // 
-            this.Edit.Location = new System.Drawing.Point(628, 59);
+            this.Edit.Location = new System.Drawing.Point(683, 54);
             this.Edit.Name = "Edit";
             this.Edit.Size = new System.Drawing.Size(75, 23);
             this.Edit.TabIndex = 3;
@@ -151,98 +160,18 @@
             this.dgvTable.AllowUserToAddRows = false;
             this.dgvTable.AllowUserToDeleteRows = false;
             this.dgvTable.AllowUserToResizeRows = false;
-            this.dgvTable.BackgroundColor = System.Drawing.SystemColors.Control;
             this.dgvTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.colWordEn,
             this.colWordRu,
             this.colType});
-            this.dgvTable.Location = new System.Drawing.Point(8, 6);
+            this.dgvTable.Location = new System.Drawing.Point(34, 31);
             this.dgvTable.MultiSelect = false;
             this.dgvTable.Name = "dgvTable";
             this.dgvTable.ReadOnly = true;
             this.dgvTable.RowHeadersVisible = false;
-            this.dgvTable.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvTable.Size = new System.Drawing.Size(533, 409);
+            this.dgvTable.Size = new System.Drawing.Size(487, 360);
             this.dgvTable.TabIndex = 2;
-            // 
-            // Delete
-            // 
-            this.Delete.Location = new System.Drawing.Point(628, 109);
-            this.Delete.Name = "Delete";
-            this.Delete.Size = new System.Drawing.Size(75, 23);
-            this.Delete.TabIndex = 1;
-            this.Delete.Text = "Удалить";
-            this.Delete.UseVisualStyleBackColor = true;
-            // 
-            // Add
-            // 
-            this.Add.Location = new System.Drawing.Point(547, 59);
-            this.Add.Name = "Add";
-            this.Add.Size = new System.Drawing.Size(75, 23);
-            this.Add.TabIndex = 0;
-            this.Add.Text = "Добавить";
-            this.Add.UseVisualStyleBackColor = true;
-            this.Add.Click += new System.EventHandler(this.Add_Click);
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.BackColor = System.Drawing.SystemColors.Control;
-            this.tabPage2.Controls.Add(this.button1);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(901, 436);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Проверка знаний";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(543, 56);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // statusStrip1
-            // 
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tssLabel});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 464);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(909, 22);
-            this.statusStrip1.TabIndex = 0;
-            this.statusStrip1.Text = "statusStrip1";
-            // 
-            // tssLabel
-            // 
-            this.tssLabel.Name = "tssLabel";
-            this.tssLabel.Size = new System.Drawing.Size(0, 17);
-            // 
-            // tscbTypeFilter
-            // 
-            this.tscbTypeFilter.FormattingEnabled = true;
-            this.tscbTypeFilter.Items.AddRange(new object[] {
-            "Сортировка по слову",
-            "Сортировка по переводу",
-            "Сортировка по типу"});
-            this.tscbTypeFilter.Location = new System.Drawing.Point(547, 6);
-            this.tscbTypeFilter.Name = "tscbTypeFilter";
-            this.tscbTypeFilter.Size = new System.Drawing.Size(149, 21);
-            this.tscbTypeFilter.TabIndex = 4;
-            // 
-            // tscbSort
-            // 
-            this.tscbSort.FormattingEnabled = true;
-            this.tscbSort.Items.AddRange(new object[] {
-            "Сортировка по слову",
-            "Сортировка по переводу",
-            "Сортировка по типу"});
-            this.tscbSort.Location = new System.Drawing.Point(718, 6);
-            this.tscbSort.Name = "tscbSort";
-            this.tscbSort.Size = new System.Drawing.Size(149, 21);
-            this.tscbSort.TabIndex = 4;
             // 
             // colWordEn
             // 
@@ -254,7 +183,6 @@
             // 
             // colWordRu
             // 
-            this.colWordRu.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.colWordRu.DataPropertyName = "WordRu";
             this.colWordRu.HeaderText = "Перевод";
             this.colWordRu.Name = "colWordRu";
@@ -262,18 +190,177 @@
             // 
             // colType
             // 
-            this.colType.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
             this.colType.DataPropertyName = "Type";
             this.colType.HeaderText = "Тип";
             this.colType.Name = "colType";
             this.colType.ReadOnly = true;
-            this.colType.Width = 51;
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(764, 54);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 1;
+            this.Delete.Text = "Удалить";
+            this.Delete.UseVisualStyleBackColor = true;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(602, 54);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 0;
+            this.Add.Text = "Добавить";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Controls.Add(this.startButton);
+            this.tabPage2.Controls.Add(this.timeLabel);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.btRun);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(922, 436);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Проверка знаний";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tssLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 464);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(930, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tssLabel
+            // 
+            this.tssLabel.Name = "tssLabel";
+            this.tssLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(158, 174);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btRun
+            // 
+            this.btRun.Location = new System.Drawing.Point(312, 113);
+            this.btRun.Name = "btRun";
+            this.btRun.Size = new System.Drawing.Size(75, 23);
+            this.btRun.TabIndex = 1;
+            this.btRun.Text = "Пуск";
+            this.btRun.UseVisualStyleBackColor = true;
+            this.btRun.Click += new System.EventHandler(this.btRun_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(183, 297);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(332, 297);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button2";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(158, 253);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(668, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(548, 90);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(30, 13);
+            this.timeLabel.TabIndex = 4;
+            this.timeLabel.Text = "Time";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(523, 116);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "startButton";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // rbSelect
+            // 
+            this.rbSelect.AutoSize = true;
+            this.rbSelect.Location = new System.Drawing.Point(17, 19);
+            this.rbSelect.Name = "rbSelect";
+            this.rbSelect.Size = new System.Drawing.Size(121, 17);
+            this.rbSelect.TabIndex = 5;
+            this.rbSelect.TabStop = true;
+            this.rbSelect.Text = "Угадывание слова";
+            this.rbSelect.UseVisualStyleBackColor = true;
+            // 
+            // rbInsert
+            // 
+            this.rbInsert.AutoSize = true;
+            this.rbInsert.Location = new System.Drawing.Point(17, 42);
+            this.rbInsert.Name = "rbInsert";
+            this.rbInsert.Size = new System.Drawing.Size(83, 17);
+            this.rbInsert.TabIndex = 5;
+            this.rbInsert.TabStop = true;
+            this.rbInsert.Text = "Ввод слова";
+            this.rbInsert.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbSelect);
+            this.groupBox1.Controls.Add(this.rbInsert);
+            this.groupBox1.Location = new System.Drawing.Point(638, 72);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(151, 67);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Режим ответов";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(909, 486);
+            this.ClientSize = new System.Drawing.Size(930, 486);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.menuStrip1);
@@ -288,8 +375,11 @@
             this.tabPage1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvTable)).EndInit();
             this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -313,12 +403,21 @@
         private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem настройкаToolStripMenuItem;
         private System.Windows.Forms.ToolStripStatusLabel tssLabel;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox tscbTypeFilter;
-        private System.Windows.Forms.ComboBox tscbSort;
         private System.Windows.Forms.DataGridViewTextBoxColumn colWordEn;
         private System.Windows.Forms.DataGridViewTextBoxColumn colWordRu;
         private System.Windows.Forms.DataGridViewTextBoxColumn colType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btRun;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbSelect;
+        private System.Windows.Forms.RadioButton rbInsert;
     }
 }
 
--- Dictionary/Main.cs
+++ Dictionary/Main.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -21,6 +22,7 @@ namespace Dictionary
         public Main()
         {
             InitializeComponent();
+            rbSelect.Checked = true;
         }
 
         private void Add_Click(object sender, EventArgs e)
@@ -101,7 +103,106 @@ namespace Dictionary
 
 
 
+
+        private void btRun_Click(object sender, EventArgs e)
+        {
+            //// Создать делегат для типа Timer.
+            //TimerCallback timeCB = new TimerCallback(TimerCount);
+            //// Установить параметры таймера.
+            //System.Threading.Timer t = new System.Threading.Timer(
+            //timeCB, // Объект делегата TimerCallback.
+            //null,   // Информация для передачи в вызванный метод (null — информация отсутствует).
+            //0,      // Период времени ожидания перед запуском (в миллисекундах).
+            //1000);  // Интервал времени между вызовами (в миллисекундах).
+            //        //Console.WriteLine("Hit key to terminate...");
+            //        //Console.ReadLine();
+
+      
+
+
+        }
+
+        //private void TimerCount(object state)
+        //{
+          
+        //    //label1.Text = count;
+        //    label1.Text = "2";
+        
+        //}
+
+        int timeLeft;
+        int addend1;
+        int addend2;
+        Random randomizer = new Random();
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                // Display the new time left by updating the Time Left label.
+                timeLeft = timeLeft - 1;
+                timeLabel.Text = timeLeft + " seconds";
+            }
+            else
+            {
+                // If the user ran out of time, stop the timer, show a MessageBox, and fill in the answers.
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry!");
+               // sum.Value = addend1 + addend2;
+                startButton.Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// Start the quiz by filling in all of the problem 
+        /// values and starting the timer. 
+        /// </summary>
+        public void StartTheQuiz()
+        {
+            //// Fill in the addition problem.
+            //// Generate two random numbers to add.
+            //// Store the values in the variables 'addend1' and 'addend2'.
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+
+            textBox1.Text = addend1.ToString();
+
+            //// Convert the two randomly generated numbers
+            //// into strings so that they can be displayed
+            //// in the label controls.
+            //plusLeftLabel.Text = addend1.ToString();
+            //plusRightLabel.Text = addend2.ToString();
+
+
+            //// 'sum' is the name of the NumericUpDown control.
+            //// This step makes sure its value is zero before
+            //// adding any values to it.
+            //sum.Value = 0;
+
+            // Start the timer.
+            timeLeft = 15;
+            timeLabel.Text = "15 seconds";
+            timer1.Start();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
+
+            if (rbSelect.Checked) { label1.Text = "Гадаем по словам"; }
+            if (rbInsert.Checked) { label1.Text = "Пишем словечки"; }
+
+
+        }
+
+
+
+
+
     }
 
 
 }
+// msdn.microsoft.com/ru-ru/library/dd492171.aspx
